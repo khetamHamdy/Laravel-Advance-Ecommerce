dd53af9aabce9642f5c0555f7b333039b1d2eab1
khetamHamdy <hamdykhetam@gmail.com>
2024-12-17 09:58:50 +0200

    solve errores in undefined var categories

--- app/Http/Controllers/Frontend/FrontendPageController.php
+++ app/Http/Controllers/Frontend/FrontendPageController.php
@@ -81,8 +81,8 @@ class FrontendPageController extends Controller
     public function subcategoryProducts($id, $slug)
     {
         $subcategory_products = Product::where('status', 1)->where('subcategory_id', $id)->orderBy('id','DESC')->paginate(3);
-        //$categories = Category::with(['subcategory'])->orderBy('category_name_en', 'ASC')->get();
-        return view('frontend.frontend_layout.subcategory_page.subcategory_product_page', compact('subcategory_products'));
+        $categories = Category::with(['subcategory'])->orderBy('category_name_en', 'ASC')->get();
+        return view('frontend.frontend_layout.subcategory_page.subcategory_product_page', compact('subcategory_products','categories'));
     }
 
     public function subsubcategoryProducts($id, $slug)
